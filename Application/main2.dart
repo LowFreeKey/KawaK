d708c516f5d2af80b188e01874f1f5eddf04384a
TheShy101 <79533966+TheShy101@users.noreply.github.com>
2021-03-22 14:53:46 +0300

    Add files via upload
    studying flutter.

---
+++ Application/main2.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Column(
+          children: [
+            Text('Hi'),
+            Text('Hello!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
